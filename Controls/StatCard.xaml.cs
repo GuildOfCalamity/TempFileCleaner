630ef3a8796ab20227cb192a378f87bc0c29bafa
GuildOfCalamity <81915293+GuildOfCalamity@users.noreply.github.com>
2026-07-25 06:40:40 -0700

    color DPs for StatCard UserControl

--- Controls/StatCard.xaml.cs
+++ Controls/StatCard.xaml.cs
@@ -22,9 +22,10 @@ namespace TempFileCleaner.Controls
             InitializeComponent();
         }
 
+        // Title
         public static readonly DependencyProperty TitleProperty =
             DependencyProperty.Register(nameof(Title), typeof(string), typeof(StatCard),
-          new PropertyMetadata(string.Empty));
+                new PropertyMetadata(string.Empty));
 
         public string Title
         {
@@ -32,6 +33,7 @@ namespace TempFileCleaner.Controls
             set => SetValue(TitleProperty, value);
         }
 
+        // Value
         public static readonly DependencyProperty ValueProperty =
             DependencyProperty.Register(nameof(Value), typeof(string), typeof(StatCard),
                 new PropertyMetadata(string.Empty));
@@ -40,6 +42,50 @@ namespace TempFileCleaner.Controls
         {
             get => (string)GetValue(ValueProperty);
             set => SetValue(ValueProperty, value);
+        }
+
+        // TitleColor
+        public static readonly DependencyProperty TitleColorProperty =
+            DependencyProperty.Register(nameof(TitleColor), typeof(Brush), typeof(StatCard),
+                new PropertyMetadata(Brushes.DodgerBlue));
+
+        public Brush TitleColor
+        {
+            get => (Brush)GetValue(TitleColorProperty);
+            set => SetValue(TitleColorProperty, value);
+        }
+
+        // ValueColor
+        public static readonly DependencyProperty ValueColorProperty =
+            DependencyProperty.Register(nameof(ValueColor), typeof(Brush), typeof(StatCard),
+                new PropertyMetadata(Brushes.White));
+
+        public Brush ValueColor
+        {
+            get => (Brush)GetValue(ValueColorProperty);
+            set => SetValue(ValueColorProperty, value);
+        }
+
+        // BackgroundColor
+        public static readonly DependencyProperty BackgroundColorProperty =
+            DependencyProperty.Register(nameof(BackgroundColor), typeof(Brush), typeof(StatCard),
+                new PropertyMetadata(new SolidColorBrush(Color.FromRgb(21, 21, 21))));
+
+        public Brush BackgroundColor
+        {
+            get => (Brush)GetValue(BackgroundColorProperty);
+            set => SetValue(BackgroundColorProperty, value);
+        }
+
+        // BorderColor
+        public static readonly DependencyProperty BorderColorProperty =
+            DependencyProperty.Register(nameof(BorderColor), typeof(Brush), typeof(StatCard),
+                new PropertyMetadata(new SolidColorBrush(Color.FromRgb(42, 127, 255))));
+
+        public Brush BorderColor
+        {
+            get => (Brush)GetValue(BorderColorProperty);
+            set => SetValue(BorderColorProperty, value);
         }
     }
 }
